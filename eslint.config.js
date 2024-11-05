458add2ee2c96b61c929a516c802871e97fd9eb5
munkeawtoast <munkeawtoast@duck.com>
2024-11-05 10:45:40 +0700

    chore: flatconfig

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+const { FlatCompat } = require('@eslint/eslintrc')
+const js = require('@eslint/js')
+const nxEslintPlugin = require('@nx/eslint-plugin')
+const typescriptEslintEslintPlugin = require('@typescript-eslint/eslint-plugin')
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+module.exports = [
+  js.configs.recommended,
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  {
+    plugins: {
+      '@nx': nxEslintPlugin,
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map(config => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map(config => ({
+    ...config,
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  ...compat.config({ env: { jest: true } }).map(config => ({
+    ...config,
+    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  { ignores: ['**/dist'] },
+]
