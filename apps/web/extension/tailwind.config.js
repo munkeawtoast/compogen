083ced9e09557c2a6b73539ad27cf6e1aecfc849
munkeawtoast <munkeawtoast@duck.com>
2024-11-05 17:11:11 +0700

    fix: tailwind configs

--- apps/web/extension/tailwind.config.js
+++ apps/web/extension/tailwind.config.js
@@ -1,15 +1,12 @@
 const baseConfig = require('../../../tailwind.config')
 const { createGlobPatternsForDependencies } = require('@nx/vue/tailwind')
 const { join } = require('path')
+const _ = require('lodash')
 
-console.error(...createGlobPatternsForDependencies(__dirname))
-console.error('hello')
-
-module.exports = {
+const a = _.mergeWith(baseConfig, {
   content: [
-    ...(baseConfig?.content || []),
     join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{vue,ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  ...baseConfig,
-}
+})
+module.exports = a
